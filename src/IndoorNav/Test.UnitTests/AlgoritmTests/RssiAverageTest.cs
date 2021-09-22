3d60afa663e85892f47a6935985bf61b639bc9d8
Aldmi1988 <aldmi@bk.ru>
2021-09-22 16:57:14 +0700

    Add RssiAverageTest

---
+++ src/IndoorNav/Test.UnitTests/AlgoritmTests/RssiAverageTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ApplicationCore.Shared.Algoritms;
+using Xunit;
+
+namespace Test.Beacons.AlgoritmTests
+{
+    public class RssiAverageTest
+    {
+        //private List<int> RssiBufer = new() {-52, -53, -55, -54, -70};
+        private List<int> RssiBufer = new() {-72, -73, -75, -74, -58};
+        
+        /// <summary>
+        /// Считать лутчше среднее Rssi 
+        /// </summary>
+        [Fact]
+        public void CalcAverageRssi()
+        {
+           var averageRssi= RssiBufer.Average();
+           var dist=Rssi2DistanceAlgoritm.CalculateDistance(-50, averageRssi);
+
+           var distList = RssiBufer.Select(rssi => Rssi2DistanceAlgoritm.CalculateDistance(-50, rssi)).ToList();
+           var dist2 = distList.Average();
+        }
+    }
+}
